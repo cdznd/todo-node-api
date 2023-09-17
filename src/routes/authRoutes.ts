3e7475ba52c3e420c0d2d3a8ad9167a3460a5056
Fabricio Gabriel <floyd9732@gmail.com>
2023-09-17 07:51:36 -0300

    Fixing lint

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,20 +1,10 @@
-import { Router, Request, Response } from "express";
-import { signup, login, logout } from "../controllers/authController";
+import { Router, type RequestHandler } from 'express'
+import { signup, login, logout } from '../controllers/authController'
 
 const router = Router()
 
-router.post('/signup', signup)
-
-router.post('/login', login)
-
-router.get('/test', (req: Request, res: Response) => {
-
-    const token = req.cookies.jwt
-    
-    res.json({token})
-
-});
-
-router.get('/logout', logout)
+router.post('/signup', signup as RequestHandler)
+router.post('/login', login as RequestHandler)
+router.get('/logout', logout as RequestHandler)
 
 export const authRoutes = router
