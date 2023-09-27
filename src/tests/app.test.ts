54780c3bf30770ec13639296655918dabd933a2a
Fabricio Gabriel <floyd9732@gmail.com>
2023-09-27 18:32:59 -0300

    adding app testing file

---
+++ src/tests/app.test.ts
@@ -0,0 +1,10 @@
+import { createServer } from '../utils/appServer';
+import express from 'express';
+
+describe('testing express app instance', () => {
+    it('testing express app instance', () => {
+        // not sure how to procced yet
+        // const app = createServer()
+        // expect(app).toBeInstanceOf(express())
+    })
+})
