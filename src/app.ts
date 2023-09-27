b75a920bd3db531201a88eb72bf5be43742faeb2
Fabricio Gabriel <floyd9732@gmail.com>
2023-09-27 18:26:22 -0300

    cleaning app file

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,9 @@
-import express, { NextFunction, type Request, type Response } from 'express'
-import mongoose from 'mongoose'
-import cookieParser from 'cookie-parser'
+import { createServer } from './utils/appServer'
+import { dbConnect } from './utils/dbServer'
+import { PORT } from './config'
 
-import { PORT, MONGO_DB_URL } from './config'
-
-import { authRoutes } from './routes/authRoutes'
-import { ticketRoutes } from './routes/ticketRoutes'
-import { checkUser, requireAuth } from './middleware/authMiddleware'
-import { handleErrors } from './middleware/errorHandlerMiddleware'
-
-// Init
-export const app = express()
-
-// Middlewares
-app.use(express.json())
-app.use(cookieParser())
-
-// DB Connection and Application start
-mongoose.connect(MONGO_DB_URL).then(() => {
-  console.log('Connected to MongoDB successfully!')
-  app.listen(PORT, () => {
-    console.log('Express application started successfully!')
-  })
-}).catch(err => {
-  console.log('Some error happened when trying to connect to the DB.')
-  console.log(err)
+const app = createServer()
+app.listen(PORT, async () => {
+  console.log(`App is running on port ${PORT}`)
+  await dbConnect()
 })
-
-app.use('*', checkUser)
-
-// Middlewares using external routes.
-app.use(authRoutes)
-app.use(ticketRoutes)
-
-// Defining an Error Handler Middleware.
-app.use(handleErrors)
