7d71500ba11c3153bfb1c7b4c9172fff13726dda
Fabricio Gabriel <floyd9732@gmail.com>
2023-09-28 19:26:26 -0300

    lint fix 3

--- src/utils/appServer.ts
+++ src/utils/appServer.ts
@@ -1,22 +1,15 @@
-import express from 'express'
+import express, { type Express } from 'express'
 import cookieParser from 'cookie-parser'
 import { handleErrors } from '../middleware/errorHandlerMiddleware'
 import { checkUser } from '../middleware/authMiddleware'
 import { appRoutes } from '../routes/appRoutes'
 
-export const createServer = () => {
-
-    const app = express()
-
-    app.use(express.json())
-    app.use(cookieParser())
-
-    app.use('*', checkUser)
-
-    appRoutes(app)
-    
-    app.use(handleErrors)
-
-    return app
-
+export const createServer = (): Express => {
+  const app = express()
+  app.use(express.json())
+  app.use(cookieParser())
+  app.use('*', checkUser)
+  appRoutes(app)
+  app.use(handleErrors)
+  return app
 }
