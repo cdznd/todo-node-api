83577a34763d389b3d0d4c25d00c1e4aa317f549
Fabricio Gabriel <floyd9732@gmail.com>
2023-09-28 19:22:25 -0300

    lint fix 2

--- src/utils/dbServer.ts
+++ src/utils/dbServer.ts
@@ -1,11 +1,10 @@
-import { MONGO_DB_URL } from "../config";
-import mongoose from "mongoose";
+import { MONGO_DB_URL } from '../config'
+import mongoose from 'mongoose'
 
-export const dbConnect = async () => {
-    try {
-        mongoose.connect(MONGO_DB_URL)
-        console.log('DB Connected')
-    } catch(err) {
-        console.log('Error trying to connect to db')
-    }
+export const dbConnect = async (): Promise<void> => {
+  try {
+    await mongoose.connect(MONGO_DB_URL)
+  } catch (err) {
+    console.log('Error trying to connect to db')
+  }
 }
