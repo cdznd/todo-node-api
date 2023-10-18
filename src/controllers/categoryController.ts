24218817390249770b617f818757a1eb839ef377
Fabricio Gabriel <floyd9732@gmail.com>
2023-10-18 19:26:03 -0300

    Using pagination shared function

--- src/controllers/categoryController.ts
+++ src/controllers/categoryController.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, type Request, type Response } from 'express'
 import { CategoryModel } from '../models/Category'
 import mongoose from 'mongoose'
+import { paginateResults } from '../utils'
 
 
 
@@ -23,25 +24,8 @@ interface listCategoriesQueryParams {
 }
 
 export const listCategories = async (req: Request, res: Response, next: NextFunction) => {
-    const { page = 1, limit = 10 }: listCategoriesQueryParams = req.query
-    const skipOffset = page > 1 ? (page - 1) * limit : 0
-
-    const totalCategories = await CategoryModel.countDocuments()
-
-    const totalPages: any = Math.ceil(totalCategories / limit)
-    const prevPage = page > 1 ? page - 1 : null;
-    const nextPage = page < totalPages ? page + 1 : null;
-
-    let links: any = {};
-
-    const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`
-
-    if (prevPage) {
-        links.prev = `${fullUrl}/?page=${prevPage}&limit=${limit}`
-    }
-    if (nextPage) {
-        links.next = `${fullUrl}/?page=${nextPage}&limit=${limit}`
-    }
+    
+    const { meta, links, limit, skipOffset } = await paginateResults(CategoryModel, req)
 
     try {
         const categories = await CategoryModel
@@ -51,9 +35,9 @@ export const listCategories = async (req: Request, res: Response, next: NextFunc
         const responseBody = {
             links,
             meta: {
-                totalItems: totalCategories,
-                totalPages,
-                page,
+                totalItems: meta.totalItems,
+                totalPages: meta.totalPages,
+                page: meta.page,
             },
             data: categories
         }
