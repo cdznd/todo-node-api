1547c8cdea2f8e6ef6b93e3fa6a19a379214e9c0
Fabricio Gabriel <floyd9732@gmail.com>
2023-11-05 19:07:08 -0300

    Adding OpenApi docs for both Category and Ticket Schema

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -5,6 +5,36 @@ interface CategoryDocumentInterface extends Document {
   title: string
 }
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Category:
+ *       type: object
+ *       required:
+ *         - title
+ *         - created_by
+ *       properties:
+ *         title:
+ *           type: string
+ *           description: The title of the category.
+ *         created_by:
+ *           type: string
+ *           description: The user who created the category, referring to an existing user.
+ *         createdAt:
+ *           type: string
+ *           format: date-time
+ *           description: The date and time the category was created.
+ *         updatedAt:
+ *           type: string
+ *           format: date-time
+ *           description: The date and time the category was last updated.
+ *   example:
+ *     title: Sample Category
+ *     created_by: d5fE_asz // User ObjectId
+ *     createdAt: 2023-11-05T12:34:56.789Z
+ *     updatedAt: 2023-11-05T12:34:56.789Z
+ */
 const CategorySchema = new mongoose.Schema({
   title: {
     type: String,
