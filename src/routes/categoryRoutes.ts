5acdc0cb62372ab8a835e467b6b5b30dde94c9c1
Fabricio Gabriel <floyd9732@gmail.com>
2023-11-09 19:26:16 -0300

    Documenting category routes

--- src/routes/categoryRoutes.ts
+++ src/routes/categoryRoutes.ts
@@ -5,7 +5,58 @@ import { categoryEndpoints } from '../config/endpoints'
 
 const router = Router()
 
-// CREATE
+/**
+ * @openapi
+ * paths:
+ *  /categories:
+ *    post:
+ *      tags:
+ *       - Category
+ *      summary: Create a new Category
+ *      description: Create a new Category
+ *      security:
+ *        - cookieAuth: []
+ *      requestBody:   # OpenAPI 3.0 provides the requestBody keyword to describe request bodies.
+ *        description: A JSON object containing a valid title.
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                title:
+ *                  type: string
+ *              example:
+ *                title: Web Project
+ *      responses:
+ *        201:
+ *          description: >
+ *            User succefully created, the user JSON is returned on the response body
+ *            The session ID is returned in a cookie named `JSESSIONID`. You need to include this cookie in subsequent requests.
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/User'
+ *          headers:
+ *            Set-Cookie:
+ *              schema:
+ *                type: string
+ *                example: JSESSIONID=abcde12345; Path=/; HttpOnly
+ *        409:
+ *          description: >
+ *            Conflit with an already existing User.
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  errors:
+ *                    type: object
+ *                example:
+ *                  errors: { "email": "Account with this email already exists" }
+ *        #401:
+ *          #$ref: '#/components/responses/UnauthorizedError'
+ */
 router.post(categoryEndpoints.categories, requireAuth, createCategory as RequestHandler)
 // READ
 // // List Categories
