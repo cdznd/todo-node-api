9704c4b820e83a0c4109ed2cad56bc358041c698
Fabricio Gabriel <floyd9732@gmail.com>
2023-11-18 23:40:02 -0300

    Adding cors configuration to the server

--- src/utils/appServer.ts
+++ src/utils/appServer.ts
@@ -1,11 +1,15 @@
 import express, { type Express } from 'express'
 import cookieParser from 'cookie-parser'
+import cors from 'cors';
 import { handleErrors } from '../middleware/errorHandlerMiddleware'
 import { checkUser } from '../middleware/authMiddleware'
 import { appRoutes } from '../routes/appRoutes'
 
+import corsConfig from '../config/corsConfig.json'
+
 export const createServer = (): Express => {
   const app = express()
+  app.use(cors(corsConfig))
   app.use(express.json())
   app.use(cookieParser())
   app.use('*', checkUser)
