f22183d22fafc8396a42d4ad882641c06b790f4b
Fabricio Gabriel <floyd9732@gmail.com>
2025-01-08 10:28:23 -0300

    .env Clean up

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,12 +1,14 @@
 import { config } from 'dotenv'
 config({ path: './.env' })
 
+// APP
 export const PORT = process.env.PORT ?? ''
-export const SECRET_KEY = process.env.SECRET_KEY ?? ''
 
+// JWT
 export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY ?? ''
 
 export const JWT_ACCESS_TOKEN_SECRET = process.env.JWT_ACCESS_TOKEN_SECRET ?? ''
 export const JWT_REFRESH_TOKEN_SECRET = process.env.JWT_REFRESH_TOKEN_SECRET ?? ''
 
-export const JWT_ACCESS_TOKEN_TTL = process.env.JWT_ACCESS_TOKEN_TTL ?? '20s'
+export const JWT_ACCESS_TOKEN_TTL = process.env.JWT_ACCESS_TOKEN_TTL ?? ''
+export const JWT_REFRESH_TOKEN_TTL = process.env.JWT_ACCESS_TOKEN_TTL ?? ''
