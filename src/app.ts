966918fccf00ab7ad724e3d87f7413b2a3a22602
Fabricio Gabriel <floyd9732@gmail.com>
2025-01-26 13:36:27 -0300

    Updating lint feedback

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,14 @@ import { PORT } from './config/app.config'
 
 const app = createServer()
 
-dbConnect()
-
-app.listen(PORT, async () => {
-  console.log(`App is running on port ${PORT}`)
-})
+void (async () => {
+  try {
+    await dbConnect()
+    app.listen(PORT, () => {
+      console.log(`App is running on port ${PORT}`)
+    })
+  } catch (error) {
+    console.error('Failed to start the application:', error)
+    process.exit(1) // will force the process to exit as quickly as possible even if there are still asynchronous operations pending 
+  }
+})()
